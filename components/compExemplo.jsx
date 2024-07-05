08f4d162ff7c05c0dae64b8faed218b20d09097d
tecmarcelo78 <tecmarcelo78@outlook.com>
2024-07-05 11:53:57 -0300

    insert component test

---
+++ components/compExemplo.jsx
@@ -0,0 +1,30 @@
+import {} from React;
+export default function compExemplo() {
+    return (
+        <View>
+            <Text style={StyleSheet.container}>Happy people
+            
+            </Text>
+        </View>
+      
+    );
+        
+    
+    
+}
+const styles = StyleSheet.create ({
+    container: {
+        alignItens:    'center',
+        justifyContent: 'center',
+        padding: '24px'
+    },
+    paragraph: {
+        margin: 24,
+        marginTop 
+    },
+    logo:{
+        width: 100,
+        height: 100,
+    },
+
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+//import './components/compExemplo';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <compExemplo>
+        <h1>Exemplo de componente</h1>
+      </compExemplo>
     </div>
   );
 }
